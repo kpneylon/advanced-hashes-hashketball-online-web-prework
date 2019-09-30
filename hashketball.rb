0683698e1a28bf45a089ff1e7b705027c904fa35
Kenneth Neylon <kenneth_neylon@hotmail.com>
2019-09-30 03:39:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -164,7 +164,7 @@ def player_numbers(team_name)
   nums = []
   game_hash.each do |place, team|
     if team[:team_name] == team_name
-      team.each do |attribute, data|
+      team.each do |attributes, data|
         if attributes == :players
           data.each do |player|
             nums << player[:number]
